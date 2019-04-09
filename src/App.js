dc362ea27cb1d36208f5c8779d0d8a7cadd635bf
NightlyD3V <TylerSpaulding95@gmail.com>
2019-04-09 05:16:45 +0100

    Learned redux basics by refactoring app to use redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
+import { createStore } from 'redux'; //import redux store
+// import reducer 
+import reducer from './reducers';
+
+//initialState i.e initial deposit 
+const initialState = {tech: "React"};
+//create the new store (bank vault)
+const store = createStore(reducer, initialState); //incomplete for now
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
+  // state object removed.
   render() {
-    return <HelloWorld tech={this.state.tech} />;
+    return <HelloWorld tech={store.getState().tech} />;
   }
 }
 
